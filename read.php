53b219e41a1586f311f159d1ad669c1c0ccc993f
ztrue <gztrue@gmail.com>
2017-02-23 19:22:03 +0200

    Fixes

--- read.php
+++ read.php
@@ -1,10 +1,8 @@
 <?php
-$file = "test.txt";
-$content = file_get_contents($file);
-$rows = explode(PHP_EOL, $content);
+$file = __DIR__ . "/input.txt";
+$rows = file($file);
 $arr = [];
 foreach ($rows as $rawRow) {
     $arr[] = explode(' ', $rawRow);
 }
 print_r($arr);
-
