cc19e1ab2968e169a9160a2f26909e7acf5927cf
ztrue <gztrue@gmail.com>
2017-02-23 20:54:56 +0200

    RUN

--- run.php
+++ run.php
@@ -1,17 +1,18 @@
 <?php
 
-require_once __DIR__ . 'parser.php';
-require_once __DIR__ . 'result.php';
-require_once __DIR__ . 'calc.php';
-require_once __DIR__ . 'Balancer.php';
+require_once __DIR__ . '/parser.php';
+require_once __DIR__ . '/result.php';
+require_once __DIR__ . '/calc.php';
+require_once __DIR__ . '/Balancer.php';
 
 $filename = __DIR__ . '/' . $argv[1];
 $algo = $argv[2];
 
-list($endpoints, $videos, $cacheSize, $cacheCount) = getVideosEndpoints($filename);
+list($endpoints, $videos, $cacheSize, $cacheCount) = getData($filename);
 
 $balancer = new Balancer($cacheCount, $cacheSize, $videos, $endpoints);
-$result = $balancer->$algo();
+$balancer->$algo();
+$result = $balancer->getResult();
 
 $calc = new Calculator($endpoints, $result);
 $score = $calc->getScore();
